121ccdf9ccb6337a6f4749e1be0b9902db3da533
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2016-03-03 17:09:49 -0600

    Alright let return back those lint changes

--- src/MountedTraversal.js
+++ src/MountedTraversal.js
@@ -216,7 +216,7 @@ export function buildInstPredicate(selector) {
           // selector is a string. match to DOM tag or constructor displayName
           return inst => instHasType(inst, selector);
       }
-      break;
+
     case 'object':
       if (!Array.isArray(selector) && selector !== null && !isEmpty(selector)) {
         return node => instMatchesObjectProps(node, selector);
